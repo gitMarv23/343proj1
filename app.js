206cc1eaf959ed56d717d3bc0175cbaa33d424d0
doubleapple123 <leonychen123@gmail.com>
2021-02-16 14:11:26 -0800

    update

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();  // Init an Express object.
+app.use(express.static(__dirname + '/'));
+app.get('/', function (req, res) { // Set page-gen fcn for URL root request.
+    res.sendFile('D:\\CollegeStuff\\Spring 2021\\CECS 343\\lab1\\js-1.html')
+});
+
+app.get('/get_form_text', function(req, res) {
+    var myText = req.query.cmdIn;
+    console.log(myText);
+    res.redirect('/');
+});
+
+app.listen(3000, function () { // Set callback action fcn on network port.
+    console.log('App.js listening on port 3000!');
+});
