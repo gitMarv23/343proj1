621a726144bd0aa818bd844d26ed0b88f5d23350
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2021-05-02 20:00:08 -0700

    finalized create manifest

--- app.js
+++ app.js
@@ -108,7 +108,7 @@ function getTextCheckSum(text){
 //create manifest file
 app.get('/create', (req, res) => {
     console.log("creating new repository");
-    makeManifestFile('repos', 'repos', "create")
+    makeManifestFile('repos', 'repos', 'create', 1)
     res.redirect("/");
 });
 
